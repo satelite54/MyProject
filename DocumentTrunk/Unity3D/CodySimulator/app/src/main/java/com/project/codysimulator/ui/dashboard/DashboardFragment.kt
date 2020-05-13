184df94adad7f3a992009343becd4ca169d44cf4
satelite54 <taehune94@gmail.com>
2020-05-13 17:07:29 +0900

    [CodySimulator]  - imageView 드래그 구현

--- DocumentTrunk/Unity3D/CodySimulator/app/src/main/java/com/project/codysimulator/ui/dashboard/DashboardFragment.kt
+++ DocumentTrunk/Unity3D/CodySimulator/app/src/main/java/com/project/codysimulator/ui/dashboard/DashboardFragment.kt
@@ -1,7 +1,9 @@
 package com.project.codysimulator.ui.dashboard
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -9,6 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.project.codysimulator.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class DashboardFragment : Fragment() {
 
@@ -22,10 +25,6 @@ class DashboardFragment : Fragment() {
         dashboardViewModel =
                 ViewModelProviders.of(this).get(DashboardViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        dashboardViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 }
--- DocumentTrunk/Unity3D/CodySimulator/app/src/main/java/com/project/codysimulator/ui/home/HomeFragment.kt
+++ DocumentTrunk/Unity3D/CodySimulator/app/src/main/java/com/project/codysimulator/ui/home/HomeFragment.kt
@@ -1,7 +1,9 @@
 package com.project.codysimulator.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
@@ -9,6 +11,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.project.codysimulator.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -22,10 +25,34 @@ class HomeFragment : Fragment() {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        imageView.setOnTouchListener{v: View, event: MotionEvent ->
+            val pWidth = (v.parent as ViewGroup).width
+            val pHeight = (v.parent as ViewGroup).height
+
+            if (event.action == MotionEvent.ACTION_MOVE) {
+                v.x = v.x + event.x - v.width / 2
+                v.y = v.y + event.y - v.height / 2
+            } else if (event.action == MotionEvent.ACTION_UP) {
+                Log.d("bsjbsj", "detached...")
+                Log.d("bsjbsj", "v.x : ${v.x} + v.y : ${v.y} , v.x + v.width : ${v.x + v.width}, v.y + y.width : ${v.y + v.width}")
+                if (v.x < 0) {
+                    v.x = 0F
+                } else if (v.x + v.width > pWidth) {
+                    v.x = (pWidth - v.width).toFloat()
+                }
+                if (v.y < 0) {
+                    v.y = 0F
+                } else if (v.y + v.height > pHeight) {
+                    v.y = (pHeight - v.height).toFloat()
+                }
+            }
+            true
+        }
     }
 }
